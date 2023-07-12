5ed5e123e3f56face0ef714649f2d0197ed936f6
Dave Noles <dave@noles.co.uk>
2023-07-12 21:13:05 +0100

    Handle single stack sites

--- app.js
+++ app.js
@@ -11,20 +11,31 @@ app.get('/ipaddresses', async (req, res) => {
         return res.status(400).send('Missing hostname parameter.');
     }
 
+    let ipv4Addresses = [];
+    let ipv6Addresses = [];
+
     try {
-        let ipv4Addresses = await dns.resolve4(hostname);
-        let ipv6Addresses = await dns.resolve6(hostname);
-        
-        let allIPs = [...ipv4Addresses, ...ipv6Addresses];
-        // Remove duplicates
-        allIPs = [...new Set(allIPs)];
-        
-        res.set('Content-Type', 'text/plain');
-        res.send(allIPs.join('\n'));
+        ipv4Addresses = await dns.resolve4(hostname);
     } catch (err) {
-        console.error(err);
-        res.status(500).send('Failed to resolve IP addresses.');
+        console.warn(`Failed to resolve IPv4 addresses for ${hostname}.`);
     }
+
+    try {
+        ipv6Addresses = await dns.resolve6(hostname);
+    } catch (err) {
+        console.warn(`Failed to resolve IPv6 addresses for ${hostname}.`);
+    }
+
+    if (!ipv4Addresses.length && !ipv6Addresses.length) {
+        return res.status(404).send(`No IP addresses found for ${hostname}.`);
+    }
+    
+    let allIPs = [...ipv4Addresses, ...ipv6Addresses];
+    // Remove duplicates
+    allIPs = [...new Set(allIPs)];
+    
+    res.set('Content-Type', 'text/plain');
+    res.send(allIPs.join('\n'));
 });
 
 app.listen(port, () => {
